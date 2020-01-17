14b6651c183b8e2cb4319d8cd05b54ef462d75d3
dan <dan@localhost.localdomain>
2020-01-17 14:38:05 +0300

    create the bussiness model class

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -2,10 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
-
-class Neighbour(models.Model):
-    name = models.CharField(max_length=150)
-
 class Profile(models.Model):
     '''
     this is a class that defines the profile structure of our user
@@ -14,9 +10,14 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to= 'images/', default= 'default.jpg')
     bio = HTMLField()
     neighbourhood = models.CharField(max_length=200)
-    
-    
 
-    
-    
+class Business(models.Model):
+    '''
+    this class gives a blueprint how our bussinesses will be made
+    '''
+    posted_by = models.ForeignKey(User,on_delete = models.CASCADE)
+    location = models.CharField(max_length=100)
+    email = models.EmailField()
+    name = models.CharField(max_length=250)
+    description = HTMLField()
 

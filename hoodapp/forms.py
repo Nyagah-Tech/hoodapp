072dacf55dc0335c0cc227ee8622ff477127d713
dan <dan@localhost.localdomain>
2020-01-17 22:19:08 +0300

    create the post form that is used to post a new post

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
-from .models import Business
+from .models import *
 
 class BusinessForm(forms.ModelForm):
     class Meta:
@@ -8,4 +8,13 @@ class BusinessForm(forms.ModelForm):
         exclude = [
             'neighbourhood',
             'posted_by'
+        ]
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = [
+            'posted_by',
+            'posted_on',
+            'neighbourhood',
         ]

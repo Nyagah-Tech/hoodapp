ea99cd32c97de5099cbf5a6b2b08c4f76f89d196
dan <dan@localhost.localdomain>
2020-01-17 22:20:51 +0300

    create a url that links us with the view function for creatineg a new post

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.home,name='home'),
     path('add/bussiness/',views.add_bussiness,name = 'add-bussiness'),
+    path('new/post/',views.new_post,name = 'new-post'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)

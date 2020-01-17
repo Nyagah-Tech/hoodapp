9cd6d073e537ac93c72ad64eee9e13333641e63e
dan <dan@localhost.localdomain>
2020-01-17 16:11:53 +0300

    create a url that links a user with the add new bussiness view function

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -4,7 +4,8 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('add/bussiness/',views.add_bussiness,name = 'add-bussiness'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)

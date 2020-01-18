578a30bb25c4c9b27ac7a23a3fe34fffbc10baa7
dan <dan@localhost.localdomain>
2020-01-18 13:55:19 +0300

    creatte urls that connect a user to the dashboard admin

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -8,7 +8,14 @@ urlpatterns = [
     path('add/bussiness/',views.add_bussiness,name = 'add-bussiness'),
     path('new/post/',views.new_post,name = 'new-post'),
     path('profile/',views.profile,name="profile"),
-    path('update/profile/',views.update_profile,name="update-profile")
+    path('update/profile/',views.update_profile,name="update-profile"),
+    #dasboard
+    path("dashboard/",views.dashboard, name="user_dashboard"),
+    path("users/", views.registered_users,name = 'system_users'),
+    path("user/activate/<int:user_id>",views.user_activate,name="activate_user"),
+    path("user/deactivate/<int:user_id>",views.user_deactivate,name="deactivate_user"),
+    
+    
     
 ]
 if settings.DEBUG:

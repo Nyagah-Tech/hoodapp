0f980373f7eaef1c6b838c4dfdfcb5c03dee1999
dan <dan@localhost.localdomain>
2020-01-18 19:10:57 +0300

    create a url that links a user with the view function that displays one neighbourhood and its details

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('profile/',views.profile,name="profile"),
     path('update/profile/',views.update_profile,name="update-profile"),
     path('logout/',views.logout_view,name="logout"),
+    path('neighbourhood/<int:id>',views.location_view, name="location"),
     #dasboard
     path("dashboard/",views.dashboard, name="user_dashboard"),
     path("users/", views.registered_users,name = 'system_users'),
